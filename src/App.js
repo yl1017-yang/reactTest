0c7d58843526ff5744edd383073156d025572ccb
yanglae <yanglae@hanmail.net>
2022-10-13 17:53:57 +0900

    7강

--- src/App.js
+++ src/App.js
@@ -70,8 +70,38 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
 
+
+      <Modal></Modal>
+
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>모달내용 함수사용하기</h2>
+      <p>
+        return() 안엔 태그 하나만 들어갈 수 있습니다. , fragments라는 문법으로 <> </>로 묶어줘도 됨 <br/>
+        이렇게 축약한 HTML 덩어리를 Component 라고 칭함
+      </p>
+
+      <div>
+        <br/>
+        <h4>Component특징 정리</h4>
+        <ul>
+          <li>1. Component 작명시 영어대문자 시작</li>
+          <li>return () 안엔 태그들이 평행하게 여러개 들어갈 수 없음 <br/>
+            1 평행하게 여러개의 태그를 쓰고 싶으면 div로 묶거나 <br/>
+            2 <> </> 이걸로 묶음</li>
+          <li>component 위치는 function App(){} 과 보통 나란히 만들어줌</li>
+          <li>Modal 이라는 컴포넌트가 App(){} 안에 있는 state를 사용하고 싶을 때 그냥 바로 쓸 수 없다는 것. props 사용</li>
+        </ul>
+      </div>
+      <p>참고사이트 : https://velog.io/@ksung1889/React-6.-%EB%A7%8E%EC%9D%80-div%EB%93%A4%EC%9D%84-%ED%95%9C-%EB%8B%A8%EC%96%B4%EB%A1%9C-%EC%A4%84%EC%9D%B4%EA%B3%A0-%EC%8B%B6%EC%9D%80-%EC%B6%A9%EB%8F%99%EC%9D%B4-%EB%93%A4-%EB%95%8C</p>
+      <p>코딩애플내용 : https://velog.io/@abcd8637/%EC%BD%94%EB%94%A9%EC%95%A0%ED%94%8C-React-%EB%A6%AC%EC%95%A1%ED%8A%B8-%EA%B8%B0%EC%B4%88%EB%B6%80%ED%84%B0-%EC%87%BC%ED%95%91%EB%AA%B0-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8%EA%B9%8C%EC%A7%80-part.1%EB%91%90%EB%B2%88%EC%A7%B8%EC%9D%B4%EC%95%BC%EA%B8%B0</p>
+    </div>
+  )
 }
 
 export default App;
