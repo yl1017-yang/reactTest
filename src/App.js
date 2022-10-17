018f850391e6e25b1985d019cfc67780838739ff
yanglae <yanglae@hanmail.net>
2022-10-17 17:59:15 +0900

    부트스트랩

--- src/App.js
+++ src/App.js
@@ -2,12 +2,98 @@
 
 import "./assets/styles/reset.css";
 import "./assets/styles/common.css";
-import RoutesMap from "./RoutesMap";
+// import RoutesMap from "./RoutesMap"; //일반 라우터 설치
+
+//https://velog.io/@cheonjae6/15%EA%B0%95-%EC%83%88%EB%A1%9C%EC%9A%B4-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EC%83%9D%EC%84%B1-Bootstrap-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
+import {Navbar, Container, Nav, Row, Col} from 'react-bootstrap';
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from './routes/detail';
 
 function App() {
+
+    let navigate = useNavigate();
+
     return( 
-        <RoutesMap />
+        // <RoutesMap />  //일반 라우터 설치
+
+        <div className="App">
+            <Navbar bg="light" variant="light">
+                <Container>
+                <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link onClick={() => { navigate(-1) }}>Home</Nav.Link>
+                    <Nav.Link onClick={() => { navigate('detail')}}>Detail</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
+            
+            {/* <Link to='/'>홈</Link>
+            <Link to='/detail'>상세페이지</Link> */}
+            
+
+            <Routes>
+                <Route path='/' element={
+                    <>
+                        <Container>
+                        <Row>
+                            <Col sm>
+                            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"></img>
+                            <h4>상품명</h4>
+                            <p>상품설명</p>
+                            </Col>
+                            <Col sm>
+                            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%"></img>
+                            <h4>상품명</h4>
+                            <p>상품설명</p>
+                            </Col>
+                            <Col sm>
+                            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%"></img>
+                            <h4>상품명</h4>
+                            <p>상품설명</p>
+                            </Col>
+                        </Row>
+                        </Container>
+                    </>
+                }/>
+
+                
+                <Route path='/detail' element={<Detail />} />
+
+                <Route path='*' element={<div>없는 페이지임.</div>} />
+                <Route path='/about' element={<About/>}>
+                    <Route path='member' element={<div>멤버임</div>} />
+                    <Route path='location' element={<div>위치정보임</div>} />
+                </Route>
+                <Route path='/event' element={<EventPage/>}>
+                    <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}/>
+                    <Route path='two' element={<div>생일기념 쿠폰받기</div>}/>
+                </Route>
+
+
+            </Routes>
+
+        </div>
+
+
     );
 }
+
+function About() {
+    return (
+      <div>
+        <h4>회사정보임</h4>
+            <Outlet></Outlet>
+      </div>
+    );
+}
+
+  function EventPage() {
+    return (
+      <div>
+        <h4>오늘의 이벤트</h4>
+        <Outlet></Outlet>
+      </div>
+    )
+  }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 // import logo from "../assets/images/logo.png";
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 
 // 리액트 링크 : https://react-router.vlpt.us/1/03.html
 
 function Header(){
+    const activeStyle = {
+        color: 'green'
+    };
+
   return(
    <div className="top_area">
     <h1 className="logo">
@@ -13,15 +17,16 @@ function Header(){
     </h1>
       <nav className="menus">
           <ul>
-              <li className={"on"}>
-                    {/* <a href={"/#"}>Page 1</a> */}
-                    <Link to={`/reactTest/`} >메인</Link>
+              <li>
+                    {/* <Link to="/reactTest/">메인</Link> */}
+                    <Link to="/reactTest/">메인</Link>
               </li>
               <li>
-                    <Link to={`/reactTest/Page1`} >서브페이지-1</Link>
+                    <NavLink exact to="/reactTest/Page1" activeStyle={activeStyle}>서브페이지-1</NavLink>
               </li>
               <li>
-                    <Link to={`/reactTest/Page2`} >서브페이지-2</Link>
+                    {/* <Link to={`/reactTest/Page2`} >서브페이지-2</Link> */}
+                    <NavLink to="/reactTest/Page2" activeStyle={activeStyle}>서브페이지-2</NavLink>
               </li>
           </ul>
       </nav>
